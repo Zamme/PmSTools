88b8b443b95a0251e236ba3d97dbe455aa254666
Jaume Castells Carles <zammecas@gmail.com>
2025-12-26 19:22:12 +0100

    main menu added

--- PmSTools/Code2Bar.xaml.cs
+++ PmSTools/Code2Bar.xaml.cs
@@ -1,3 +1,6 @@
+using CommunityToolkit.Maui.Core;
+using Plugin.Maui.OCR;
+
 namespace PmSTools;
 
 public partial class Code2Bar : ContentPage
@@ -5,5 +8,56 @@ public partial class Code2Bar : ContentPage
     public Code2Bar()
     {
         InitializeComponent();
+    }
+    
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+
+    }
+        
+    private async void OnReadocrClicked()
+    {
+        try
+        {
+            var pickResult = await MediaPicker.Default.CapturePhotoAsync();
+                
+            if (pickResult != null)
+            {
+                using var imageAsStream = await pickResult.OpenReadAsync();
+                var imageAsBytes = new byte[imageAsStream.Length];
+                await imageAsStream.ReadAsync(imageAsBytes);
+
+                var ocrResult = await OcrPlugin.Default.RecognizeTextAsync(imageAsBytes);
+
+                if (!ocrResult.Success)
+                {
+                    await DisplayAlert("No success", "No OCR possible", "OK");
+                    return;
+                }
+
+                /*await DisplayAlert("OCR Result", ocrResult.AllText, "OK");*/
+                MauiPopup.PopupAction.DisplayPopup(new PopupPage(ocrResult.AllText));
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+
+    private void OnBackHomeFromCode2BarClicked(object? sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+
+    protected async void InitOcrAsync()
+    {
+        await OcrPlugin.Default.InitAsync();
+    }
+    private void OnScanNewCodeButtonClicked(object? sender, EventArgs e)
+    {
+        InitOcrAsync();
+        OnReadocrClicked();
     }
 }
--- PmSTools/MainPage.xaml.cs
+++ PmSTools/MainPage.xaml.cs
@@ -1,104 +1,34 @@
 ﻿using CommunityToolkit.Maui.Core;
-using Plugin.Maui.OCR;
 
 namespace PmSTools
 {
     public partial class MainPage : ContentPage
     {
-        private ICameraProvider cameraProvider;
 
         public MainPage(ICameraProvider cameraProvider)
         {
             InitializeComponent();
-
-            /*this.cameraProvider = cameraProvider;*/
         }
 
         protected async override void OnAppearing()
         {
             base.OnAppearing();
 
-            await OcrPlugin.Default.InitAsync();
-            
-            OnReadocrClicked();
-        }
-        
-        // Implemented as a follow up video https://youtu.be/JUdfA7nFdWw
-        /*protected async override void OnNavigatedTo(NavigatedToEventArgs args)
-        {
-            base.OnNavigatedTo(args);
-            
-            await cameraProvider.RefreshAvailableCameras(CancellationToken.None);
-            MyCamera.SelectedCamera = cameraProvider.AvailableCameras
-                .Where(c => c.Position == CameraPosition.Rear).FirstOrDefault();
-        }*/
-
-        // Implemented as a follow up video https://youtu.be/JUdfA7nFdWw
-        /*protected override void OnNavigatedFrom(NavigatedFromEventArgs args)
-        {
-            base.OnNavigatedFrom(args);
-
-            MyCamera.MediaCaptured -= MyCamera_MediaCaptured;
-            MyCamera.Handler?.DisconnectHandler();
-        }*/
-
-        /*private void MyCamera_MediaCaptured(object? sender, CommunityToolkit.Maui.Views.MediaCapturedEventArgs e)
-        {
-            if (Dispatcher.IsDispatchRequired)
-            {
-                Dispatcher.Dispatch(() => MyImage.Source = ImageSource.FromStream(() => e.Media));
-                return;
-            }
-
-            MyImage.Source = ImageSource.FromStream(() => e.Media);
-        }*/
-
-        private async void OnReadocrClicked()
-        {
-            /*this.TakePhoto();*/
-            try
-            {
-                var pickResult = await MediaPicker.Default.CapturePhotoAsync();
-                
-                if (pickResult != null)
-                {
-                    using var imageAsStream = await pickResult.OpenReadAsync();
-                    var imageAsBytes = new byte[imageAsStream.Length];
-                    await imageAsStream.ReadAsync(imageAsBytes);
-
-                    var ocrResult = await OcrPlugin.Default.RecognizeTextAsync(imageAsBytes);
-
-                    if (!ocrResult.Success)
-                    {
-                        await DisplayAlert("No success", "No OCR possible", "OK");
-                        return;
-                    }
-
-                    /*await DisplayAlert("OCR Result", ocrResult.AllText, "OK");*/
-                    MauiPopup.PopupAction.DisplayPopup(new PopupPage(ocrResult.AllText));
-                }
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Error", ex.Message, "OK");
-            }
         }
 
-        /*private async void TakePhoto()
+        private void OnCode2BarcodeButtonClicked(object? sender, EventArgs e)
         {
-            await MyCamera.CaptureImage(CancellationToken.None);
-        }*/
-
-        /*private void Button_Clicked_1(object sender, EventArgs e)
-        {
-            MyCamera.CameraFlashMode = MyCamera.CameraFlashMode == CameraFlashMode.Off ? 
-                CameraFlashMode.On : CameraFlashMode.Off;
+            Navigation.PushAsync(new Code2Bar());
         }
 
-        private void Button_Clicked_2(object sender, EventArgs e)
+        protected async void ShowNotAvailable()
         {
-            MyCamera.ZoomFactor += 0.1f;
-        }*/
+            await DisplayAlertAsync("Ups!", "Encara no esta disponible", "OK");
+        }
+
+        private void OnFindPlaceButtonClicked(object? sender, EventArgs e)
+        {
+            ShowNotAvailable();      
+        }
     }
-
 }
